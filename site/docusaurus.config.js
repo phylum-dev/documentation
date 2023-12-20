f69cce1116a957b06dfcb83b52e816852e81be84
Charles Coggins <maxrake@users.noreply.github.com>
2023-12-20 12:37:55 -0600

    docs: update sidebar and main config

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -8,31 +8,23 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  // TODO: Change these next two values
   title: 'Phylum Documentation',
   tagline: 'The software supply chain security company',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://docs-stg.phylum.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
   trailingSlash: false,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'phylum-dev', // Usually your GitHub org/user name.
-  projectName: 'documentation', // Usually your repo name.
+  // GitHub pages deployment config
+  organizationName: 'phylum-dev',
+  projectName: 'documentation',
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
   onDuplicateRoutes: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -49,6 +41,7 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // TODO: Remove this to remove the "edit this page" links?
+          // TODO: Use a function here instead, to account for the git submodules?
           editUrl: 'https://github.com/phylum-dev/documentation/tree/main/site/',
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
@@ -69,7 +62,6 @@ const config = {
         disableSwitch: false,
         respectPrefersColorScheme: true,
       },
-      // The announcement bar can be used to highlight big changes
       announcementBar: {
         content: 'Welcome to the new Phylum CLI documentation!',
         textColor: '#fff',
@@ -90,18 +82,6 @@ const config = {
             label: 'Docs',
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'kbSidebar',
-            position: 'left',
-            label: 'KB',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'cliSidebar',
-            position: 'left',
-            label: 'CLI',
-          },
-          {
             href: 'https://api.phylum.io/api/v0/swagger/index.html',
             position: 'left',
             label: 'API',
@@ -116,65 +96,42 @@ const config = {
       },
       footer: {
         style: 'dark',
-        logo: {
-          alt: 'Phylum Logo',
-          src: 'img/phylum_logo.svg',
-          height: 100,
-          width: 100,
-          href: 'https://phylum.io',
-        },
         copyright: `Copyright © 2020-${new Date().getFullYear()} Phylum, Inc.`,
         links: [
           {
-            title: 'Docs',
-            // TODO: Ensure these `Docs` items are good
-            items: [
-              {
-                label: 'Quickstart',
-                to: '/',
-              },
-            ],
+            label: 'Phylum Home',
+            href: 'https://phylum.io',
           },
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/c9QnknWxm3',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/Phylum_IO',
-              },
-              {
-                label: 'YouTube',
-                href: 'https://www.youtube.com/@phylum_io',
-              },
-              {
-                label: 'DEV',
-                href: 'https://dev.to/phylum',
-              },
-              {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/company/phylum-io',
-              },
-            ],
+            label: 'Discord',
+            href: 'https://discord.gg/c9QnknWxm3',
           },
           {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/phylum-dev',
-              },
-            ],
+            label: 'Twitter',
+            href: 'https://twitter.com/Phylum_IO',
+          },
+          {
+            label: 'GitHub',
+            href: 'https://github.com/phylum-dev',
+          },
+          {
+            label: 'YouTube',
+            href: 'https://www.youtube.com/@phylum_io',
+          },
+          {
+            label: 'LinkedIn',
+            href: 'https://www.linkedin.com/company/phylum-io',
+          },
+          {
+            label: 'DEV',
+            href: 'https://dev.to/phylum',
           },
         ],
       },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        // The default languages are found here:
+        // The default languages for code block highlighting are found here:
         // https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9
         // Choose additional languages from here: https://prismjs.com/#supported-languages
         // When there are aliases, use the name of the first one in the list containing the entry you actually use.
@@ -212,15 +169,17 @@ const config = {
     },
   },
 
-  // This plugin is needed due to interactions between docusaurus and webpack not handling symlinks correctly. Without
-  // it, the site won't build due to failures in following the symlinks in `docs` to their git submodules. References:
+  // This plugin is needed due to interactions between docusaurus and webpack not
+  // handling symlinks correctly. Without it, the site won't build due to failures
+  // in following the symlinks in `docs` to their git submodules. References:
   // https://github.com/facebook/docusaurus/issues/3272#issuecomment-876374383
   // https://github.com/facebook/docusaurus/issues/6257
+  // https://docusaurus.io/docs/api/plugin-methods
   plugins: [
-    function (context, options) {
+    async function (context, options) {
       return {
         name: 'webpack-configuration-plugin',
-        configureWebpack(config, isServer, utils) {
+        configureWebpack(config, isServer, utils, content) {
           return {
             resolve: {
               symlinks: false,
